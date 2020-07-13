dc685a4d43d4981f2c5bbe9025fae1427e9c1dae
Omari Brown <omaris.brown@gmail.com>
2020-07-13 14:21:45 -0400

    struggled to get image to show on page

--- src/App.js
+++ src/App.js
@@ -66,6 +66,8 @@ class App extends React.Component {
             <h4>We offer a range of preventative and wellness services - including exams, vaccinations, nutrition, and more. We can also treat your pet should they become sick from illness or injury. At this time, we do not see critical and/or emergency care, though there are several clinics we work with throughout the area that can provide these services if needed.</h4>
             <h3>Wellness Exams</h3>
             <h4>Preventative medicine is the key to keeping your pet happy and healthy. We believe that all pets should undergo a comprehensive wellness exam annually, which focuses on all body systems and helps us identify health conditions in your pet before they become difficult and expensive to treat. These exams enable us to create an overall picture of your pet's health status, combining aspects of your pet's at home routine and history with physical exam findings to create straightforward recommendations for your pet's continued wellbeing.</h4>
+
+            {/* <img src='./assets/dog.png' alt="image of dig"/> */}
           </div>
 
           <div className='right-div'>
